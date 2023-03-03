68db8568a7fc5c46bc208886403de7684312b7cf
Jorge García <jorgemgr94@gmail.com>
2023-03-03 01:49:02 -0600

    test updating images domains

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	reactStrictMode: true
+	reactStrictMode: true,
+  images: {
+    domains: ['personal-portfolio-main-yj436fhcma-uc.a.run.app'],
+  },
 };
 
 module.exports = nextConfig;
