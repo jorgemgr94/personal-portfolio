b1dac260002ce2de81c6bc17298b623223145b5e
Jorge García <jorgemgr94@gmail.com>
2025-06-14 09:38:56 -0600

    chore: upgrade ESLint configuration and Next.js config files, update project name and version in package files

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: ['personal-portfolio-main-yj436fhcma-uc.a.run.app']
-  }
-};
-
-module.exports = nextConfig;
