2085e0961b857577ee35fbbb5005841e2738329c
Jorge García <jorgemgr94@gmail.com>
2025-09-20 15:27:26 -0600

    feat: update NavbarV1 constants to include Projects section and remove Knowledge Areas

--- src/components/NavbarV1/constants.ts
+++ src/components/NavbarV1/constants.ts
@@ -2,7 +2,7 @@ import type { NavigationItem } from './types';
 
 export const navigationItems = [
   { name: 'About Me', section: '#about' },
-  { name: 'Knowledge Areas', section: '#knowledge' },
   { name: 'Professional Experience', section: '#experience' },
+  { name: 'Projects', section: '#projects' },
   { name: 'Resume', href: '/resume', target: '_blank' },
 ] satisfies NavigationItem[];
