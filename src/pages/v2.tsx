c61e477bdc3dfdab779fe9e7a8f1ca2af2747deb
Jorge García <jorgemgr94@gmail.com>
2025-09-21 13:28:13 -0600

    feat: integrate AboutMe, WorkExperience, and Projects components into V2 layout

--- src/pages/v2.tsx
+++ src/pages/v2.tsx
@@ -1,9 +1,15 @@
+import AboutMe from '@/components/AboutMe';
 import Navbar from '@/components/NavbarV1';
+import WorkExperience from '@/components/ProfessionalExperience/ProfessionalExperience';
+import Projects from '@/components/Projects';
 
 function V2() {
   return (
-    <main className="font-light">
+    <main className="font-light text-pretty">
       <Navbar />
+      <AboutMe />
+      <WorkExperience />
+      <Projects />
     </main>
   );
 }
