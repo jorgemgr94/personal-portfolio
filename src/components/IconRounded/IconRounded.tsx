ad82ca5bdb3d18ea9bbe931e859c9e3a2c23e5b1
Jorge García <jorgemgr94@gmail.com>
2025-09-23 17:26:25 -0600

    refactor: remove IconRounded component and its related files for a cleaner codebase

--- src/components/IconRounded/IconRounded.tsx
+++
@@ -1,7 +0,0 @@
-export default function IconRounded ({ icon }: { icon: React.ReactNode }) {
-  return (
-    <span className="inline-flex items-center justify-center mr-4 h-12 w-12 rounded-full bg-gray-700 hover:bg-gray-500 text-white">
-      {icon}
-    </span>
-  );
-}
