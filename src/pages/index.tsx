a3d7a3807c93b8211b4d6801f3cced44a23586e7
Jorge García <jorgemgr94@gmail.com>
2025-09-23 02:10:19 -0600

    feat: deprecate index in favor of v2

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,19 @@
-import About from '@/components/About';
-import Experience from '@/components/Experience';
+import AboutMe from '@/components/AboutMe';
+import Navbar from '@/components/NavbarV1';
+import WorkExperience from '@/components/ProfessionalExperience/ProfessionalExperience';
+import Projects from '@/components/Projects';
 import Head from 'next/head';
-import Navbar from '@/components/Navbar';
-import Social from '@/components/Social';
-import TechStack from '@/components/TechStack';
-import ThemeToggler from '@/components/ThemeToggler';
 
-export default function Index () {
+export default function Index() {
   return (
-    <main className="flex flex-col md:flex-row">
+    <main className="font-light text-pretty">
       <Head>
         <title>Jorge García - Software Engineer</title>
       </Head>
-      <ThemeToggler />
       <Navbar />
-      <section className="basis-full dark:bg-gray-800 dark:text-white md:basis-9/12 md:ml-64 xl:basis-10/12">
-        <About />
-        <TechStack />
-        <Experience />
-        <Social />
-      </section>
+      <AboutMe />
+      <WorkExperience />
+      <Projects />
     </main>
   );
 }
