1516a4f1b457ee899ae58d62c5524e7dca62dbf2
Jorge García <jorgemgr94@gmail.com>
2025-09-23 17:28:44 -0600

    refactor: remove ALink, BlogArticle, Experience, Footer, SectionTemplate, and TechStack components along with their related files for a cleaner codebase

--- src/data/index.ts
+++
@@ -1,6 +0,0 @@
-export { about } from './about';
-export { positions } from './positions';
-export { socialNetworks } from './socialNetworks';
-export { technologies } from './technologies';
-export { CompaniesImages, GeneralImages, SocialIconsImages } from './types';
-export type { IPosition } from './types';
