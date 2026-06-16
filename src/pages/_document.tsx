47a27c111f0fc31eadec89946577110a51c2b875
Jorge Garcia <jorgemgr94@gmail.com>
2026-06-16 15:47:43 -0600

    chore: update open graph and twitter metadata image format to webp

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -20,7 +20,7 @@ export default function Document() {
         <meta property="og:url" content="https://jorgemgr.com/" />
         <meta
           property="og:image"
-          content="https://jorgemgr.com/assets/images/me.jpg"
+          content="https://jorgemgr.com/assets/images/me.webp"
         />
         <meta
           property="og:description"
@@ -40,7 +40,7 @@ export default function Document() {
         />
         <meta
           name="twitter:image"
-          content="https://jorgemgr.com/assets/images/me.jpg"
+          content="https://jorgemgr.com/assets/images/me.webp"
         />
 
         {/* Canonical */}
